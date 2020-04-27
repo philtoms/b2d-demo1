82f575c063cd90f50e7a5f04af2aea2c6c7faba2
Phil Toms <phil.toms@hotmail.co.uk>
2020-04-27 17:13:33 +0100

    basic setup for import-map

---
+++ server/import-map.js
@@ -0,0 +1,9 @@
+export default `<script defer src="/node_modules/es-module-shims/dist/es-module-shims.js"></script>
+<script type="importmap-shim">
+{
+  "imports": {
+    "vh-check": "/src/vh-check.js",
+    "scroll-next": "/src/scroll-next.js"
+  }
+}
+</script>`;
